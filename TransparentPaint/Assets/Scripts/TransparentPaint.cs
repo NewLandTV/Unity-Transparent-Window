eb9822f4803f1469e2a887f7a7e8ca109d897bfa
JkhTV <candzzang5283@gmail.com>
2024-09-07 07:30:03 +0900

    Add transparent paint project

---
+++ TransparentPaint/Assets/Scripts/TransparentPaint.cs
@@ -0,0 +1,120 @@
+using System;
+using System.Collections;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TransparentPaint : MonoBehaviour
+{
+    [SerializeField]
+    private RectTransform target;
+    [SerializeField]
+    private Image backgroundImage;
+    [SerializeField]
+    private AudioSource clickSound;
+
+    public struct Margins
+    {
+        public int leftWidth;
+        public int rightWidth;
+        public int topHeight;
+        public int bottomHeight;
+    }
+
+    [DllImport("User32.dll")]
+    public static extern IntPtr GetActiveWindow();
+
+    [DllImport("User32.dll")]
+    public static extern int SetWindowLong(IntPtr hWnd, int nIndex, uint dwNewLong);
+
+    [DllImport("User32.dll")]
+    public static extern int BringWindowToTop(IntPtr hWnd);
+
+    [DllImport("User32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int x, int y, int cx, int cy, uint uFlags);
+
+    [DllImport("Dwmapi.dll")]
+    public static extern uint DwmExtendFrameIntoClientArea(IntPtr hWnd, ref Margins margins);
+
+    private static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
+    private static readonly IntPtr HWND_NOTOPMOST = new IntPtr(-2);
+
+    private IntPtr hWnd;
+    private const UInt32 SWP_NOSIZE = 0x0001;
+    private const UInt32 SWP_NOMOVE = 0x0002;
+
+    private const int GWL_EXSTYLE = -20;
+    private uint WS_EX_LAYERED = 0x00080000;
+    private const uint WS_EX_TRANSTPARENT = 0x00000020;
+
+    private void Awake()
+    {
+        Application.runInBackground = true;
+        Cursor.visible = false;
+
+        hWnd = GetActiveWindow();
+
+        Margins margins = new Margins { leftWidth = -1 };
+
+        DwmExtendFrameIntoClientArea(hWnd, ref margins);
+        SetWindowLong(hWnd, GWL_EXSTYLE, WS_EX_LAYERED);
+        BringWindowToTop(hWnd);
+        SetWindowPos(hWnd, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOSIZE);
+    }
+
+    private IEnumerator Start()
+    {
+        StartCoroutine(PaintCoroutine());
+
+        bool toggle = true;
+
+        while (true)
+        {
+            target.position = Input.mousePosition;
+
+            if (Input.GetKeyDown(KeyCode.T))
+            {
+                toggle = !toggle;
+
+                BringWindowToTop(hWnd);
+                SetWindowPos(hWnd, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOSIZE);
+
+                if (toggle)
+                {
+                    SetWindowLong(hWnd, GWL_EXSTYLE, WS_EX_LAYERED);
+                }
+                else
+                {
+                    SetWindowLong(hWnd, GWL_EXSTYLE, WS_EX_LAYERED | WS_EX_TRANSTPARENT);
+                }
+            }
+
+            yield return null;
+        }
+    }
+
+    private IEnumerator PaintCoroutine()
+    {
+        float alpha = 0f;
+
+        while (true)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                clickSound.Play();
+            }
+
+            alpha += 4f * Time.deltaTime;
+
+            if (alpha >= 1f)
+            {
+                alpha = 0f;
+            }
+
+            backgroundImage.color = new Color(1f, 1f, 1f, alpha);
+
+            yield return null;
+        }
+    }
+}
